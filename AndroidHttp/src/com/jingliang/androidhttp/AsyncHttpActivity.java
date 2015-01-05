aaf5564e2567d9efc39a859c84fcc7b56b6b02aa
tangjingliang <jingliang.tang@hampoo.com>
2015-01-05 10:17:09 +0800

    jingliang add by asynchttp first

---
+++ AndroidHttp/src/com/jingliang/androidhttp/AsyncHttpActivity.java
@@ -0,0 +1,110 @@
+package com.jingliang.androidhttp;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AsyncHttpActivity extends Activity {
+	
+	private static final String URL = "http://www.baidu.com";
+	private TextView resultText;
+	private ProgressBar requestProgress;
+
+	private AsyncHttpClient mClient = new AsyncHttpClient();
+
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_asynchttp);
+
+		resultText = (TextView) findViewById(R.id.text_result);
+		requestProgress = (ProgressBar) findViewById(R.id.progress_request);
+
+		mClient.setTimeout(10000);
+		mClient.get(URL, new MyTextHandler());
+		requestProgress.setVisibility(View.VISIBLE);
+	};
+
+	private void progressDismiss() {
+		requestProgress.setVisibility(View.GONE);
+	}
+
+	private void showFailure() {
+		Toast.makeText(this, "Request Failure!", Toast.LENGTH_SHORT).show();
+	}
+
+	class MyResponHandler extends AsyncHttpResponseHandler {
+
+		@Override
+		public void onFailure(int arg0, Header[] arg1, byte[] arg2,
+				Throwable arg3) {
+			// TODO Auto-generated method stub
+			try {
+				String str = new String(arg2);
+				resultText.setText(str);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			progressDismiss();
+			showFailure();
+		}
+
+		@Override
+		public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+			// TODO Auto-generated method stub
+
+			progressDismiss();
+		}
+
+	}
+
+	class MyTextHandler extends TextHttpResponseHandler {
+
+		@Override
+		public void onFailure(int arg0, Header[] arg1, String arg2,
+				Throwable arg3) {
+			// TODO Auto-generated method stub
+
+			progressDismiss();
+			showFailure();
+		}
+
+		@Override
+		public void onSuccess(int arg0, Header[] arg1, String arg2) {
+			// TODO Auto-generated method stub
+			resultText.setText(arg2);
+
+			progressDismiss();
+		}
+
+	}
+
+	class MyJsonHandler extends JsonHttpResponseHandler {
+		@Override
+		public void onSuccess(int statusCode, Header[] headers,
+				JSONArray response) {
+			// TODO Auto-generated method stub
+			super.onSuccess(statusCode, headers, response);
+			progressDismiss();
+		}
+
+		@Override
+		public void onFailure(int statusCode, Header[] headers,
+				String responseString, Throwable throwable) {
+			// TODO Auto-generated method stub
+			super.onFailure(statusCode, headers, responseString, throwable);
+			progressDismiss();
+			showFailure();
+		}
+	}
+}
---
+++ AndroidHttp/src/com/jingliang/androidhttp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.jingliang.androidhttp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity {
+
+	private Intent mIntent;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		findViewById(R.id.button_asynchttp).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						mIntent = new Intent(MainActivity.this,
+								AsyncHttpActivity.class);
+						startActivity(mIntent);
+					}
+				});
+	}
+}
